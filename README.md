715c9fd855577748b34ffd29ad99f28ca1c58465
Hang Zhao <hangzhao@HangdeMacBook-Pro.local>
2022-12-19 17:46:01 +0800

    add genres

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # allen_web
 
 https://zhuanlan.zhihu.com/p/47962034
-https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Home_page
+
+tutorial_web: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Home_page
+
 https://github.com/mdn/django-locallibrary-tutorial/blob/main/catalog/
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Book, Author, BookInstance
+from .models import Book, Author, BookInstance, Genre
 
 
 def index(request):
@@ -16,11 +16,14 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_genres = Genre.objects.count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres
     }
 
     # Render the HTML template base_generic.html with the data in the context variable
